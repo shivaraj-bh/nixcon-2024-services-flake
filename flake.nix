b1614844d4aeec79a139609965a0cf1c642316e2
shivaraj-bh <me@shivaraj-bh.in>
2024-10-19 16:48:11 +0530

    presentation: init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Material from my NixCon 2024 talk on `services-flake`";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { pkgs, lib, ... }: {
+        apps.default.program = lib.getExe (pkgs.writeShellApplication {
+          name = "start-presentation";
+          runtimeInputs = [ pkgs.presenterm ];
+          text = ''
+            exec presenterm ${inputs.self}/presentation.md
+          '';
+        });
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.presenterm ];
+        };
+      };
+    };
+}
